7cf05de74a246804bd4e1090798a2e6d0e7881c4
Mihmet Akpinar <mihmet.akpinar@check24.de>
2024-06-05 09:04:22 +0200

    fix rubocop linting

--- lib/facter/vault_version.rb
+++ lib/facter/vault_version.rb
@@ -5,11 +5,11 @@
 # Purpose: Retrieve vault version if installed
 #
 Facter.add(:vault_version) do
-    setcode do
-        if Facter::Util::Resolution.which('vault')
-            vault_server_version_output = Facter::Util::Resolution.exec('vault version')
-            match = vault_server_version_output.match(%r{Vault v(\d+\.\d+\.\d+)})
-            match&.captures.first
-        end
+  setcode do
+    if Facter::Util::Resolution.which('vault')
+      vault_server_version_output = Facter::Util::Resolution.exec('vault version')
+      match = vault_server_version_output.match(%r{Vault v(\d+\.\d+\.\d+)})
+      match&.captures&.first
     end
+  end
 end
