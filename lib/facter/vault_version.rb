2ce7d3891e45183eaa6a8d474888c3877306b73d
Mihmet Akpinar <mihmet.akpinar@check24.de>
2024-06-05 08:53:30 +0200

    Fixed issue #29 that version is not updated when parameter is changing

---
+++ lib/facter/vault_version.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# Fact: vault_version
+#
+# Purpose: Retrieve vault version if installed
+#
+Facter.add(:vault_version) do
+    setcode do
+      if Facter::Util::Resolution.which('vault')
+        vault_server_version_output = Facter::Util::Resolution.exec('vault version')
+        match = vault_server_version_output.match(%r{Vault v(\d+\.\d+\.\d+)})
+        match ? match.captures.first : nil
+      end
+    end
+  end
+  
