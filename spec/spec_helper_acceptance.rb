7fcfbbda881ae182e5a6b7ac3c5babfc5190d021
Mihmet Akpinar <mihmet.akpinar@check24.de>
2024-06-05 08:53:30 +0200

    modulesync 9.1.0
    This is a cleanup for https://github.com/voxpupuli/puppet-vault/pull/52

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -5,7 +5,6 @@
 
 require 'voxpupuli/acceptance/spec_helper_acceptance'
 
-ENV['BEAKER_FACTER_VAULT_VERSION'] = '1.12.0'
 configure_beaker(modules: :metadata)
 
 Dir['./spec/support/acceptance/**/*.rb'].sort.each { |f| require f }
