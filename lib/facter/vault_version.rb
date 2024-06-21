408dec4a8a57d53314d3627fe802aeb77b907d4b
Mihmet Akpinar <mihmet.akpinar@check24.de>
2024-06-21 12:54:04 +0200

    use confine instead of if statement

--- lib/facter/vault_version.rb
+++ lib/facter/vault_version.rb
@@ -5,11 +5,10 @@
 # Purpose: Retrieve vault version if installed
 #
 Facter.add(:vault_version) do
+  confine {Facter::Util::Resolution.which('vault')}
   setcode do
-    if Facter::Util::Resolution.which('vault')
-      vault_server_version_output = Facter::Util::Resolution.exec('vault version')
-      match = vault_server_version_output.match(%r{Vault v(\d+\.\d+\.\d+)})
-      match&.captures&.first
-    end
+    vault_server_version_output = Facter::Util::Resolution.exec('vault version')
+    match = vault_server_version_output.match(%r{Vault v(\d+\.\d+\.\d+)})
+    match&.captures&.first
   end
 end
