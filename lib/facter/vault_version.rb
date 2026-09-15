6f03b929a489dea3f56204387f0a08b8e56145e8
Corporate Gadfly <haroon.rafique@gmail.com>
2026-09-15 17:22:07 -0400

    replace deprecated calls with Facter::Core::Execution
    Signed-off-by: Corporate Gadfly <haroon.rafique@gmail.com>

--- lib/facter/vault_version.rb
+++ lib/facter/vault_version.rb
@@ -5,9 +5,9 @@
 # Purpose: Retrieve vault version if installed
 #
 Facter.add(:vault_version) do
-  confine { Facter::Util::Resolution.which('vault') }
+  confine { Facter::Core::Execution.which('vault') }
   setcode do
-    vault_server_version_output = Facter::Util::Resolution.exec('vault version')
+    vault_server_version_output = Facter::Core::Execution.execute('vault version')
     match = vault_server_version_output.match(%r{Vault v(\d+\.\d+\.\d+)})
     match&.captures&.first
   end
